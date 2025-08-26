b056aaa43ba2b42e0493dc12912a0a7a0747e09d
Carlos Henrique Ziegler de Araujo <chziegler445@gmail.com>
2025-08-25 22:52:15 -0300

    Refactor code structure for improved readability and maintainability

--- apps/api-gateway/src/seed/seed.module.ts
+++ apps/api-gateway/src/seed/seed.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { BusinessCategoryAndServiceTypeSeed } from './business-category-and-service-type.seed';
+import { MailTemplatesSeed } from './mail-templates.seed';
 import { SeedProfessionalPlansUseCase } from './plans.seed';
 
 @Module({
-  providers: [BusinessCategoryAndServiceTypeSeed, SeedProfessionalPlansUseCase],
-  exports: [BusinessCategoryAndServiceTypeSeed, SeedProfessionalPlansUseCase],
+  providers: [
+    BusinessCategoryAndServiceTypeSeed,
+    SeedProfessionalPlansUseCase,
+    MailTemplatesSeed,
+  ],
+  exports: [
+    BusinessCategoryAndServiceTypeSeed,
+    SeedProfessionalPlansUseCase,
+    MailTemplatesSeed,
+  ],
 })
 export class SeedModule {}
