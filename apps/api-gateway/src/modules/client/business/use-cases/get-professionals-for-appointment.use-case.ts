a56def3c1b58456647892f861724d64267f9ca5d
Carlos Henrique Ziegler de Araujo <chziegler445@gmail.com>
2025-10-06 23:33:41 -0300

    feat: enhance GetProfessionalsForAppointmentUseCase with FileSystemService for profile image URLs

--- apps/api-gateway/src/modules/client/business/use-cases/get-professionals-for-appointment.use-case.ts
+++ apps/api-gateway/src/modules/client/business/use-cases/get-professionals-for-appointment.use-case.ts
@@ -1,10 +1,14 @@
 import { PrismaService } from '@app/shared';
+import { FileSystemService } from '@app/shared/services';
 import { Injectable } from '@nestjs/common';
 import { GetProfessionalsForAppointmentDto } from '../dto/requests/get-professionals.dto';
 
 @Injectable()
 export class GetProfessionalsForAppointmentUseCase {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly fs: FileSystemService,
+  ) {}
 
   async execute(query: GetProfessionalsForAppointmentDto) {
     const professionals = await this.prismaService.professionalProfile.findMany(
@@ -30,7 +34,9 @@ export class GetProfessionalsForAppointmentUseCase {
     return professionals?.map((professional) => ({
       id: professional.id,
       name: professional.User?.name?.split(' ')[0] || '',
-      profile_image: professional.profile_image,
+      profile_image: this.fs.getPublicUrl({
+        key: professional.profile_image,
+      }),
     }));
   }
 }
