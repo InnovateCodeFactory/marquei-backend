8bb9f7e5c600ce7663a7452484efa3b93d94ed32
Carlos Henrique Ziegler de Araujo <chziegler445@gmail.com>
2025-12-09 11:41:45 -0300

    feat: Update deployment workflow and Docker Compose configuration for improved image management and container orchestration

--- run/prod-up.sh
+++ run/prod-up.sh
@@ -1,1 +1,13 @@
-docker compose -f docker-compose.prod.yml up -d --build
+#!/usr/bin/env bash
+set -e
+
+cd /var/www/marquei/backend
+
+# Atualiza código (compose, .env, etc)
+git pull
+
+# Puxa imagens mais recentes do GHCR
+docker compose -f docker-compose.prod.yml pull
+
+# Sobe containers usando as novas imagens
+docker compose -f docker-compose.prod.yml up -d --remove-orphans
