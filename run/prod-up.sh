7d7a8c82a2c879fc2787b26661800ba276b08f39
Carlos Henrique Ziegler de Araujo <chziegler445@gmail.com>
2025-12-09 12:03:03 -0300

    feat: Add image cleanup step to production deployment script

--- run/prod-up.sh
+++ run/prod-up.sh
@@ -11,3 +11,6 @@ docker compose -f docker-compose.prod.yml pull
 
 # Sobe containers usando as novas imagens
 docker compose -f docker-compose.prod.yml up -d --remove-orphans
+
+# Limpa imagens que não estão sendo usadas por nenhum container
+docker image prune -af
